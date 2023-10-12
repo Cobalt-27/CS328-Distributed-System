97c961825022bb7322806a65257cd9c0fb766259
Luo Jiajun <34511059+Cobalt-27@users.noreply.github.com>
2023-10-12 11:27:57 +0000

    modify matmul shell script

--- matmul/run.sh
+++ matmul/run.sh
@@ -3,28 +3,50 @@
 # Compile the code
 mpic++ matmul.cpp -o matmul.out
 
-output_file="test_results.txt"
+# Output file
+output_file="test_results.log"
 
-echo "" > $output_file
+# Argument parsing for number of experiments
+n=1
+while getopts ":n:" opt; do
+  case $opt in
+    n) n="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
 
-echo "-----------------------------"
-echo "| np  | Distributed |   BF   |"
-echo "-----------------------------"
+# Clean the output file
+> $output_file
 
-for np in 1 2 4 8 16 32; do
-    result=$(mpirun -np $np --oversubscribe ./matmul.out 2>&1)
-    np_space=$(printf "%-4s" $np)
-    distributed_time=$(echo $result | awk '{print $2}')
-    bf_time=$(echo $result | awk '{print $3}')
-    distributed_space=$(printf "%-10s" $distributed_time)
-    bf_space=$(printf "%-4s" $bf_time)
-
-    # For console display in table format
-    echo "| $np_space | $distributed_space | $bf_space |"
+# Run the entire experiment for n times
+for exp in $(seq 1 $n); do
+    echo "Experiment run: $exp"
+    
+    # Print table header for console output
+    echo "-----------------------------"
+    echo "| np  | Distributed |   BF   |"
     echo "-----------------------------"
 
-    # For log file in simpler format
-    echo "$np $distributed_time $bf_time" >> $output_file
+    # Run tests for np = 1, 2, 4, ..., 32
+    for np in 1 2 4 8 16 32; do
+        result=$(mpirun -np $np --oversubscribe ./matmul.out 2>&1)
+        np_space=$(printf "%-4s" $np)
+        distributed_time=$(echo $result | awk '{print $2}')
+        bf_time=$(echo $result | awk '{print $3}')
+        distributed_space=$(printf "%-10s" $distributed_time)
+        bf_space=$(printf "%-4s" $bf_time)
+        echo "| $np_space | $distributed_space | $bf_space |"
+        echo "-----------------------------"
+        
+        # If it's the first run, log the results
+        if [ "$exp" -eq 1 ]; then
+            echo "$np $distributed_time $bf_time" >> $output_file
+        fi
+
+        sleep 0.2
+    done
 done
 
 echo "Tests completed!"
