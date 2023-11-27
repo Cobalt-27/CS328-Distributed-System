ecb77f2f4f0fff9ab003f12f36e8fd35bf3163a8
Luo Jiajun <34511059+Cobalt-27@users.noreply.github.com>
2023-11-27 13:19:24 +0000

    remove redundant test class

--- MyRMI/src/main/java/myrmi/tests/SimpleRemoteInterface.java
+++
@@ -1,8 +0,0 @@
-package myrmi.tests;
-
-import myrmi.Remote;
-import myrmi.exception.RemoteException;
-
-public interface SimpleRemoteInterface extends Remote {
-    String sayHello() throws RemoteException;
-}
